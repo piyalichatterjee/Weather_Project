7b07481ecb47a3a8337c1286e6b1914971d9753c
piyalichatterjee <piyalichatterjeedeveloper@gmail.com>
2025-05-20 12:11:44 +0530

    create project

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+function App() {
+  const queryClient = new QueryClient()
+  return (
+     <QueryClientProvider client={queryClient}>
+    <div className='text-8xl text-red-500 underline'>
+      hello
+      <Button variant={'destructive'} onClick={()=>console.log("this is my project")}>click me</Button>
+    </div>
+       <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
+}
+
+export default App
